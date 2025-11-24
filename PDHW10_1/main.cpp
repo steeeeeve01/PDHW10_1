129e2ccbb5de8f3ee28aff2bac688ceeebfa5099
steeeeeve01 <136960831+steeeeeve01@users.noreply.github.com>
2025-11-24 15:52:05 +0800

    Initial Commit

---
+++ PDHW10_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PDHW10_1
+//
+//  Created by 簡紹益 on 2025/11/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
